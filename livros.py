ad78682a17adbf8bfed05d7269587a5ae746f519
Emanuelle Alves <122374698+emanuelle4lves@users.noreply.github.com>
2024-11-18 16:25:12 -0300

    Create livros.py
    Atividade avaliativa, Alysson Daniel e Emanuelle Alves (INFO2V)

---
+++ livros.py
@@ -0,0 +1,32 @@
+class Livro:
+    def __init__(self, titulo: str, qtd_paginas: int, paginas_lidas: int):
+        self.titulo = titulo
+        self.qtd_paginas = qtd_paginas
+        self.paginas_lidas = paginas_lidas
+
+    # Métodos getters e setters
+    def get_titulo(self) -> str:
+        return self.titulo
+
+    def set_titulo(self, titulo: str) -> None:
+        self.titulo = titulo
+
+    def get_qtd_paginas(self) -> int:
+        return self.qtd_paginas
+
+    def set_qtd_paginas(self, qtd_paginas: int) -> None:
+        self.qtd_paginas = qtd_paginas
+
+    def get_paginas_lidas(self) -> int:
+        return self.paginas_lidas
+
+    def set_paginas_lidas(self, paginas_lidas: int) -> None:
+        self.paginas_lidas = paginas_lidas
+
+    # Método para verificar o progresso
+    def verificar_progresso(self) -> None:
+        if self.qtd_paginas == 0:
+            print("O livro não tem páginas registradas.")
+        else:
+            progresso = (self.paginas_lidas / self.qtd_paginas) * 100
+            print(f"Você já leu {progresso:.2f}% do livro.")
